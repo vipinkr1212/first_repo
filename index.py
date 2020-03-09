cd13a3279bc530e1fc6f89250ba8af12dab2be4c
vipinkr1212 <vipinkr1212@gmail.com>
2020-03-10 00:19:18 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,15 @@
+# /**
+# git remote add origin https://github.com/vipinkr1212/first_repo.git
+# git push -u origin master
+
+
+# git add .
+# git commit -m "some message"
+
+# git push origin master
+
+
+
+# **/
+
+
